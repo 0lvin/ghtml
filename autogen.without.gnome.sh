9c951ba27142ac300a2451793358091a5d6b49f7
Denis Pauk <pauk.denis@gmail.com>
2009-10-17 13:48:11 +0300

    add autogen without gnome-common

---
+++ autogen.without.gnome.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+test ! -e INSTALL &&touch INSTALL
+test ! -e NEWS &&touch NEWS
+test ! -e README &&touch README
+test ! -e AUTHORS &&touch AUTHORS
+test ! -e ChangeLog &&touch ChangeLog
+test ! -e COPYING &&touch COPYING
+
+test ! -e configure.ac && autoscan && echo AM_INIT_AUTOMAKE >>configure.scan && nano configure.scan && mv configure.scan configure.ac 
+
+libtoolize --copy --force &&
+glib-gettextize -f &&
+intltoolize --force &&
+aclocal &&
+autoconf &&
+autoheader &&
+automake --add-missing &&
+./configure &&
+make
